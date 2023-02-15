abda1aae476142380cfe457e8ae4ff5d9a74e7f6
ranjani <shiva.ranjani83@gmail.com>
2023-02-15 15:42:37 -0500

    Added Log files, parameterized testNG.

--- src/test/java/dataUsaAPITest.java
+++ src/test/java/dataUsaAPITest.java
@@ -1,61 +1,56 @@
 import static io.restassured.RestAssured.*;
-
-import com.beust.ah.A;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.json.simple.JSONObject;
-import org.testng.Assert;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.hamcrest.Matchers.*;
+
+
 public class dataUsaAPITest {
-    @Test
-    public void getPopYr(){
+
+    @DataProvider
+    public Object[][] data(){
+        return new Object[][] {{"2020",322903030}};
+    }
+    private final static Logger logger = LogManager.getLogger(dataUsaAPITest.class);
+
+
+    @Test(dataProvider = "data")
+    public void getPopYr(String s1, int p1)  {
         baseURI = "https://datausa.io/api";
-//        given().
-//                get("data?drilldowns=Nation&measures=Population").
-//                then().
-//                statusCode(200);
+        Response response = given().get("data?drilldowns=Nation&measures=Population");
 
+        JsonPath extractor = response.jsonPath();
 
-      Response response = given().get("data?drilldowns=Nation&measures=Population");
-//      response.prettyPrint();
+        List<Object> responseData = extractor.getList("data");
 
-      JsonPath extractor = response.jsonPath();
-//
-//        Object obj = extractor.get("data.Year,Population");
-//        System.out.println(obj.toString());
+       logger.info("Response Data is" + responseData);
 
-        System.out.println(extractor.getList("data"));
-      ArrayList<String> year = new ArrayList();
+        ArrayList<String> year = new ArrayList();
         ArrayList<Integer> population = new ArrayList();
-    year = extractor.get("data.Year");
-    population = extractor.get("data.Population");
-        System.out.println(year);
-        System.out.println(population);
 
+        year = extractor.get("data.Year");
+        population = extractor.get("data.Population");
 
-        for(String yearCheck : year){
-            if(yearCheck.equals("2020") ){
-                System.out.println("Year is present");
+       logger.info("Year details" + year);
+       logger.info("Population details" + population);
+
+        for (String yearCheck : year) {
+            if (yearCheck.equals(s1)) {
+               logger.info("Year is present");
+
             }
         }
 
-        for(int popCheck : population){
-            if(popCheck == 322903030){
-                System.out.println("Pop is present");
+        for (int popCheck : population) {
+            if (popCheck == p1) {
+                logger.info("Population is present");
+
             }
         }
-
-        JSONObject obj1 = new JSONObject();
-//     obj1 = extractor.getJsonObject("data");
-
-        Assert.assertEquals(year.get(0), "2020");
-//    Response res1 = extractor.get("data[].Year");
-
-//        System.out.println(res1);
-
     }
 }
